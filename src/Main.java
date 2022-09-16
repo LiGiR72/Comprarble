7a45bc624337d356160529023f85dcfc245b53a1
LiGiR72 <iliya.alabyew@gmail.com>
2022-09-16 16:44:13 +0300

    2nd task completed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         List<Person> list = new ArrayList<>();
         list.add(new Person("Robert", "won Robertson", 47));
-        list.add(new Person("William", "won bom Wlliamson", 56));
+        list.add(new Person("William", "won bon Wlliamson", 56));
+        list.add(new Person("Test","More Then Three Words",40));
         list.add(new Person("Bob", "Ros", 35));
         list.add(new Person("Robert", "won Robertsonv2", 48));
-        list.sort(new PersonComparator());
+        Collections.sort(list,new PersonComparator(3));
         System.out.println(list);
     }
 }
--- src/PersonComparator.java
+++ src/PersonComparator.java
@@ -1,10 +1,17 @@
 import java.util.Comparator;
 
 public class PersonComparator implements Comparator<Person> {
+    private int maxWords;
+    public PersonComparator(int maxWords){
+        this.maxWords = maxWords;
+    }
+
     @Override
     public int compare(Person o1, Person o2) {
-        int stringComp = o2.getSurname().split(" ").length - o1.getSurname().split(" ").length;
-        if(stringComp == 0){
+        int o1WordCount = o1.getSurname().split(" ").length;
+        int o2WordCount = o2.getSurname().split(" ").length;
+        int stringComp = o2WordCount - o1WordCount;
+        if(stringComp == 0 ||(o2WordCount >= maxWords && o1WordCount >= maxWords)){
             return o2.getAge() - o1.getAge();
         }
         return stringComp;
