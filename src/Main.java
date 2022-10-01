e178f90b4f194e9e290200c96f1f0d62a78fc262
LiGiR72 <iliya.alabyew@gmail.com>
2022-10-01 18:07:27 +0300

    Lambda implementation

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,22 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        int maxWords = 4;
         List<Person> list = new ArrayList<>();
         list.add(new Person("Robert", "won Robertson", 47));
         list.add(new Person("William", "won bon Wlliamson", 56));
         list.add(new Person("Test","More Then Three Words",40));
         list.add(new Person("Bob", "Ros", 35));
         list.add(new Person("Robert", "won Robertsonv2", 48));
-        Collections.sort(list,new PersonComparator(3));
+        Collections.sort(list,(Person o1, Person o2) -> {
+            int o1WordCount = o1.getSurname().split(" ").length;
+            int o2WordCount = o2.getSurname().split(" ").length;
+            int stringComp = o2WordCount - o1WordCount;
+            if(stringComp == 0 ||(o2WordCount >= maxWords && o1WordCount >= maxWords)){
+                return o2.getAge() - o1.getAge();
+            }
+            return stringComp;
+        });
         System.out.println(list);
     }
 }
