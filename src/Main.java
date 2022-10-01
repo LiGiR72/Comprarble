6c7c951d5dd9d635258d59ad45bf8431a6196f20
LiGiR72 <iliya.alabyew@gmail.com>
2022-10-01 18:55:45 +0300

    Young people has been executed

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,11 @@ public class Main {
     public static void main(String[] args) {
         List<Person> list = new ArrayList<>();
         list.add(new Person("Robert", "won Robertson", 47));
-        list.add(new Person("William", "won bon Wlliamson", 56));
+        list.add(new Person("William", "won bon Wlliamson", 16));
         list.add(new Person("Test","More Then Three Words",40));
         list.add(new Person("Bob", "Ros", 35));
-        list.add(new Person("Robert", "won Robertsonv2", 48));
+        list.add(new Person("Robert", "won Robertsonv2", 17));
+        list.removeIf( person -> person.getAge() < 18);
         Collections.sort(list,new PersonComparator(3));
         System.out.println(list);
     }
